22fb000cbad375755755a26cf644b5f95d16487b
Yomotsu <admin@yomotsu.net>
2023-06-10 11:12:17 +0900

    chore: types

--- src/types.ts
+++ src/types.ts
@@ -1,8 +1,9 @@
-export enum State {
-	WHOLE_IN,
-	PART_IN,
-	OUT,
-}
+export const State = {
+	WHOLE_IN: 0,
+	PART_IN: 1,
+	OUT: 2,
+} as const;
+export type State = typeof State[keyof typeof State];
 
 export type RootMargin = number | `${ string }px` | `${ string }%`;
 
